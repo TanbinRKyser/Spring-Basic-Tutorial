ba232552e4070e9adf5a7f80ae57aed78e800144
foxhunter24 <tmitul515@gmail.com>
2019-04-02 19:38:58 +0600

    Initial commit: Commiting all the changes - Mitul

---
+++ src/main/java/com/tutorial/WebappV1Application.java
@@ -0,0 +1,13 @@
+package com.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run( WebappV1Application.class, args );
+	}
+
+}
